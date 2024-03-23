8eee3e9a84f6789f54cffb8468bae69922aee390
KongWongsakorn <wongsakornboonchai@gmail.com>
2024-03-24 00:57:01 +0700

    Update FirstPageController.php

--- app/Http/Controllers/FirstPageController.php
+++ app/Http/Controllers/FirstPageController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class FirstPageController extends Controller
 {
+    public function index():View{
 
+        $title = ["ฺBODYPUMP","BODY COMBAT","BODY COMBAT"];
+        
+        return view('FirstPage',['title'=>$title]);
+    }
 }
